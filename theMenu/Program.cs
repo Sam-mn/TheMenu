620e006eacd348ecc0f60e7e754b752f6c607a99
Samer Munawwar <samer.mnr@gmail.com>
2024-05-21 21:33:21 +0200

    First commit

---
+++ theMenu/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace TheMenu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
